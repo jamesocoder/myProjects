0c6ab8cc935f4225a8879fe16cd398e9d72c5422
jamesocoder <44654277+jamesocoder@users.noreply.github.com>
2024-06-27 21:16:46 -0400

    Quick comment clarifications

--- spotifyApi/shuffle/front/src/App.tsx
+++ spotifyApi/shuffle/front/src/App.tsx
@@ -5,7 +5,10 @@ import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import {Login} from './components/authComp';
 
 function App() {
-  /* Record frontend's address
+  /* TODO: Record frontend's address to Redux state.  Need this to be available
+  for components to reference.  Both Vite and React environment variables can't
+  be edited at runtime like we do on the backend with raw dotenv.
+  
   import.meta.env.VITE_ADDR = (() => {
     let serverAddr = import.meta.env.DEV ? "http" : "https";
     serverAddr += `://${import.meta.env.VITE_HOST}:${import.meta.env.VITE_PORT}`;
--- spotifyApi/shuffle/front/src/utils.ts
+++ spotifyApi/shuffle/front/src/utils.ts
@@ -1,3 +1,6 @@
+/* Helps capture hard-to-catch errors that disappear upon navigating
+to a different site, which clears the console of any printed errors.
+Let's the user save the error to a text file. */
 export function saveErrToTxt(err: any) {
     let strErr = JSON.stringify(err, Object.getOwnPropertyNames(err));
     let blob = new Blob([strErr], {type: 'text/plain;charset=utf-8'});
