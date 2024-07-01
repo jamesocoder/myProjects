2ad66720b1336198c06f36f10a899174f161f289
jamesocoder <44654277+jamesocoder@users.noreply.github.com>
2024-06-30 22:09:18 -0400

    Replaced child page's reliance on non-persistent state with localStorage

--- spotifyApi/shuffle/front/src/components/topArtists.tsx
+++ spotifyApi/shuffle/front/src/components/topArtists.tsx
@@ -1,27 +1,37 @@
 import axios from 'axios';
-import { useAppSelector } from '../state';
-import { AccessToken, Artist, Image } from '@spotify/web-api-ts-sdk';
+import { Artist, Image } from '@spotify/web-api-ts-sdk';
 import { useEffect, useState } from 'react';
 
-/* Axios.js's GET function doesn't support sending a JSON body.  This is
-because browsers automatically ignore all GET request bodies by default.
-Although this endpoint responds to requests in a way that is typical for
-GET requests -- not POST requests -- we must use one of the request
-methods that supports a JSON body.
-https://stackoverflow.com/questions/46404051/send-object-with-axios-get-request/46404151#46404151 */
 export function TopArtists(): JSX.Element {
-    const axDefault = axios.create({
-        baseURL: import.meta.env.VITE_BACKEND
-    })
-    const token: AccessToken = useAppSelector(state => state.authorization.accessToken);
-
     const [data, setData] = useState([]);
 
+    // Example of setting a root URL for axios requests
+    const axDefault = axios.create({baseURL: import.meta.env.VITE_BACKEND});
+
+    /* Spotify's SDK doesn't have an easily accessible static getAccessToken() function.
+    In order to get access to the current one, we must first create an instance of
+    SpotifyApi with all the necessary arguments: ClientID, RedirectUri, and Scopes.
+    
+    The key for our app's token was found in:
+    https://github.com/spotify/spotify-web-api-ts-sdk/blob/main/src/auth/AuthorizationCodeWithPKCEStrategy.ts
+     - It is listed under the member variable cacheKey. */
+    let token: any = localStorage.getItem('spotify-sdk:AuthorizationCodeWithPKCEStrategy:token');
+
     useEffect(() => {
-        axDefault.post('/top-5', token)
-            .then(result => setData(result.data.items))
-            .catch(err => console.error(err));
-    }, [setData]);
+        if (token) {
+            token = JSON.parse(token);
+
+            /* Axios.js's GET function doesn't support sending a JSON body.  This is
+            because browsers automatically ignore all GET request bodies by default.
+            Although this endpoint responds to requests in a way that is typical for
+            GET requests -- not POST requests -- we must use one of the request
+            methods that supports a JSON body.
+            https://stackoverflow.com/questions/46404051/send-object-with-axios-get-request/46404151#46404151 */
+            axDefault.post('/top-5', token)
+                .then(result => setData(result.data.items))
+                .catch(err => console.error(err));
+        }
+    }, [setData, token]);
 
     return <>
         <h1>Your All-Time Top Artists</h1>
@@ -36,7 +46,9 @@ export function TopArtists(): JSX.Element {
                         width={image.width}
                         border='5px' />
                     <p>{artist.name}</p>
-                    <a href={artist.external_urls.spotify} target='_blank'>{artist.external_urls.spotify}</a>
+                    <a href={artist.external_urls.spotify} target='_blank'>
+                        {artist.external_urls.spotify}
+                    </a>
                 </tr>
             })
         }</tbody></table>
