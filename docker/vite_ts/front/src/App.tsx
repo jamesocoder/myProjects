d477bc8e127421eaa984fd23d096b2e285ba0aa0
jamesocoder <44654277+jamesocoder@users.noreply.github.com>
2025-03-03 21:25:29 -0500

    Code prettying

--- docker/vite_ts/front/src/App.tsx
+++ docker/vite_ts/front/src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css'
 import { useEffect, useState } from 'react'
 import { Environment, Header, Instructions, Secret } from './components';
-import { swapB as swapLogo } from './assets'; // Change which logo is imported here to demo Watch and HMR
+// Change which logo is imported here to see Watch and HMR
+import { swapB as swapLogo } from './assets';
 
 export default function App() {
   const [count, setCount] = useState(0);
@@ -11,15 +12,13 @@ export default function App() {
   Vite's build process */
   useEffect(() => console.log(import.meta.env), []);
 
-  return (
-    <>
-      <Header swapLogo={swapLogo} />
-      <Environment />
-      <Secret />
-      <Instructions />
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-    </>
-  )
+  return (<>
+    <Header swapLogo={swapLogo} />
+    <Environment />
+    <Secret />
+    <Instructions />
+    <button onClick={() => setCount((count) => count + 1)}>
+      count is {count}
+    </button>
+  </>)
 }
